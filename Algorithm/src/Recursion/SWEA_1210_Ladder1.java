c2299b4018f31a749f3ae172feb4c90133b7f316
ssafy <hayeo3496@gmail.com>
2023-08-02 16:58:30 +0900

    SWEA 사다리 제출

--- Algorithm/src/Recursion/SWEA_1210_Ladder1.java
+++ Algorithm/src/Recursion/SWEA_1210_Ladder1.java
@@ -17,12 +17,53 @@ import java.util.StringTokenizer;
 public class SWEA_1210_Ladder1 {
 	static BufferedReader br;
 	static StringTokenizer st;
-	static int T;
-	static int[][] board;
-	static boolean exist;
-	
+	static StringBuilder sb;
+
+	static final int T = 10;
+	static int[][] map;
+	static int DEST;
+	static int curRow, curCol;
+
+	// аб -> ©Л -> ╩С
+	static final int[][] dir = { { 0, -1 }, { 0, 1 }, { -1, 0 } };
+
 	public static void main(String[] args) throws IOException {
-		
+
+		br = new BufferedReader(new InputStreamReader(System.in));
+		map = new int[100][100];
+		sb=new StringBuilder();
+		for (int tc = 1; tc <= T; tc++) {
+			br.readLine();
+
+			for (int row = 0; row < 100; row++) {
+				st = new StringTokenizer(br.readLine());
+
+				for (int col = 0; col < 100; col++) {
+					map[row][col] = Integer.parseInt(st.nextToken());
+					if (map[row][col] == 2) {
+						DEST = col;
+					}
+				}
+			}
+			curRow = 99;
+			curCol = DEST;
+
+			while (curRow > 0) {
+				for (int d = 0; d < dir.length; d++) {
+					int nxtRow = curRow + dir[d][0];
+					int nxtCol = curCol + dir[d][1];
+					
+					if (nxtRow<0 || nxtRow>99 || nxtCol<0 || nxtCol>99) continue;
+					if (map[nxtRow][nxtCol]==0) continue;
+					map[curRow][curCol]=0;
+					curRow=nxtRow;
+					curCol=nxtCol;
+					break;
+				}
+			}
+			sb.append("#"+tc+" "+curCol+"\n");
+		}
+		System.out.println(sb);
 	}
 
 }
